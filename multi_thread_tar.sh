d6a425153e6f0e70f5bf1a477682c0838384376c
tenix7676 <tenix7676@gmail.com>
2025-08-06 00:42:01 +0200

    Fixed issue with parsing text with spaces e.g. "Hi there h"

--- multi_thread_tar.sh
+++ multi_thread_tar.sh
@@ -1,36 +1,67 @@
 #!/usr/bin/env bash
 
-_is_a_flag()
+#For DEBUGGING:
+#function list()
+#{
+#	for arg in "$@"
+#	do
+#		printf \'"${arg}"\''\n'
+#	done
+#}
+
+___is_a_flag()
 {
 	[[ "${1:0:1}" == - ]] && [[ "${1:1:1}" != - ]]
 }
 
-_flag_into_many_flags()
+___flag_into_many_flags()
 {
 	local flag="${1:1}"
 	for (( i=0; i<${#flag}; i++ )); do
-		printf " -${flag:$i:1}"
+		echo "-${flag:$i:1}"
 	done
 }
 
-_parse()
+___parse()
 {
-	for arg in "$@"; do
-		if _is_a_flag "$arg"; then
-			_flag_into_many_flags "$arg"
+	declare -n in=$1
+	declare -n out=$2	
+	for arg in "${in[@]}"
+	do
+		if ___is_a_flag "$arg"
+		then
+			out+=($(___flag_into_many_flags "$arg"))
 		else
-			printf " $arg"
+			out+=("${arg}")
 		fi
 	done
 }
 
 tar()
 {
-local ARGS=$(_parse "$@")
-local NEW_ARGS=$(echo "${ARGS}" | sed '
-	s/ -z/ --use-compress-program=pigz/g
-	s/ -j/ --use-compress-program=pbzip2/g
-	s/ -J/ --use-compress-program=pixz/g
-')
-command tar ${NEW_ARGS[@]}
+	local args=("$@")
+	local PARSED_ARGS
+	___parse args PARSED_ARGS
+	local NEW_ARGS
+	for arg in "${PARSED_ARGS[@]}"
+	do
+		local new_arg="$(printf "%s" "$arg" | sed '
+		s/-z/--use-compress-program=pigz/g
+		s/-j/--use-compress-program=pbzip2/g
+		s/-J/--use-compress-program=pixz/g
+		')"
+		NEW_ARGS+=("${new_arg}")
+	done
+	command tar ${NEW_ARGS[@]}
+
+	#For DEBUGGING:
+	#printf "\nOG:\n"
+	#list "${@}"
+	#printf "\nParsed:\n"
+	#list "${PARSED_ARGS[@]}"
+	#printf "\nNew list\n"
+	#list "${NEW_ARGS[@]}"
 }
+
+#For DEBUGGING:
+#tar -zfd gig --hello="Fasd asd ttt" pizza "dum mmy"
